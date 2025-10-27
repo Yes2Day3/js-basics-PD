3051b603def6cb6e24ba656e7e5df5da5b6e4ede
unknown <john@same.com>
2025-10-27 11:23:46 +0200

    Added array

---
+++ people-array.js
@@ -0,0 +1,17 @@
+let people = [
+    {
+        name: "Peter",
+        age: 22,
+        isStudent: true,
+    },
+    {
+        name: "Lois",
+        age: 22,
+        isStudent: false,
+    },
+]
+
+//for(let i = 0; i < people.length; i++){
+    let person = people.length
+    console.log(person.name)
+//}

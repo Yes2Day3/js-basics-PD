77ff0570a38e2d84f0996e024f9ad06b4c9f76ca
unknown <john@same.com>
2025-10-27 10:58:55 +0200

    Added conditionals

---
+++ ticket-price.js
@@ -0,0 +1,12 @@
+let visitorAge = 12;
+let isStudent = true;
+
+if(visitorAge <= 6){
+    console.log("Ieeja is bezmaksas.")
+}
+else if(isStudent = true){
+    console.log("Biļetes cena ir 5 EUR")
+}
+else{
+    console.log("Biļete ir 10 EUR")
+}
